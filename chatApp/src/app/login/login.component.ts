e6a5191df4270023df25ceec221141aac8cadbb5
Rangaiah N Vinodarao <rvinodarao001@nam.ad.pwcinternal.com>
2017-09-15 16:19:52 +0530

    committs

--- chatApp/src/app/login/login.component.ts
+++ chatApp/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule, AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
@@ -12,7 +13,7 @@ import * as firebase from 'firebase/app';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
+  user: Observable<firebase.User>;
   constructor( public afAuth: AngularFireAuth , private router: Router) {
   }
   loginFB() {
@@ -22,15 +23,28 @@ export class LoginComponent implements OnInit {
     const provider = new firebase.auth.GoogleAuthProvider();
     provider.addScope('profile');
     provider.addScope('email');
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-     // This gives you a Google Access Token.
-     const token = result.credential.accessToken;
-     // The signed-in user info.
-      const user = result.user;
-       console.log(user) ;
-       console.log(user.displayName);
+    firebase.auth().signInWithPopup(provider).then(
+      (userInfo) => {
+      // You are now logged in
+      // Maybe redirect to the first logged in page
+      const user = userInfo.user;
+      // console.log(user);
+       this.router.navigate([ 'signup' ]);
+    })
+    .catch((error) => {
+      // Ran into some issues
+      // Maybe report the error on your sign in page
     });
- }
+  //     function(result) {
+  //    // This gives you a Google Access Token.
+  //    var token = result.credential.accessToken;
+  //    // The signed-in user info.
+  //    var user = result.user;
+  //      console.log(user) ;
+  //      console.log(user.displayName);
+  //   }    
+  // );    
+  }
 
  logout() {
   this.afAuth.auth.signOut();
--- chatApp/src/app/signup/signup.component.ts
+++ chatApp/src/app/signup/signup.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 import { Router } from '@angular/router';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
+
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-signup',
@@ -7,8 +13,21 @@ import { Router } from '@angular/router';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
+  name: any;
+  state: string = '';
 
-  constructor() { }
+  constructor(public af: AngularFireAuth, private router: Router) {
+
+    this.af.auth.onAuthStateChanged(auth => {
+      if (auth) {
+        this.name = auth;
+      }
+    });
+  }
+  logout() {
+    this.af.auth.signOut();
+    this.router.navigateByUrl('/login');
+  }
 
   ngOnInit() {
   }
